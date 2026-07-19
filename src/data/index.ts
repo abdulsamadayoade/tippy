678c91777504c165f41de89daddd115e1486838e
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2026-07-19 05:38:55 +0100

    feat: add sampleCreator data structure for user profile

---
+++ src/data/index.ts
@@ -0,0 +1,11 @@
+const sampleCreator = {
+  firstName: "Abdulsamad",
+  name: "Abdulsamad Ayoade",
+  handle: "@abdullllsamad",
+  username: "abdul",
+  category: "Software Engineer",
+  bio: "Nothing really that much, this is just a sample bio",
+  tipUrl: "tippy.cash/abdul",
+};
+
+export { sampleCreator };
