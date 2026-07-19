0695a4171cb60fad9c4e1d34a6fba6b6831f9352
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2026-07-19 23:57:50 +0100

    feat: update Secured component to provide default text for better usability

--- src/components/elements/secured.tsx
+++ src/components/elements/secured.tsx
@@ -1,7 +1,7 @@
 import { LockIcon } from "../icons/lock";
 import { MonnifyLogo } from "./monnify-logo";
 
-export function Secured({ text }: { text: string }) {
+export function Secured({ text = "Payments secured by" }: { text?: string }) {
   return (
     <p className="mt-2.5 flex items-center justify-center gap-1 text-xs text-muted-text">
       <LockIcon className="size-4 stroke-2" aria-hidden="true" />
