30964a4287e9be770478a975f63166f3a7944014
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2026-07-20 00:31:35 +0100

    feat: update CloseIcon dimensions and improve SVG path for better visual clarity

--- src/components/icons/close.tsx
+++ src/components/icons/close.tsx
@@ -6,17 +6,17 @@ export function CloseIcon({ className, ...props }: ComponentProps<"svg">) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="24"
-      height="24"
+      width="32"
+      height="32"
+      color="currentColor"
       fill="none"
       stroke="currentColor"
-      strokeWidth="1.75"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       className={cn(className)}
       {...props}>
-      <path d="M6 6L18 18" />
-      <path d="M18 6L6 18" />
+      <path d="M18 6L12 12M12 12L6 18M12 12L18 18M12 12L6 6"></path>
     </svg>
   );
 }
