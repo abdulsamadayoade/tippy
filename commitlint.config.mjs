228d75217dae640b9a8b4564122dd8f5d09c55f2
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2026-09-20 17:58:01 +0100

    feat: update commitlint configuration to include header-max-length rule

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,8 @@
-export default { extends: ["@commitlint/config-conventional"] };
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 120],
+  },
+};
+
+export default config;
